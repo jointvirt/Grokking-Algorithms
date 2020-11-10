12c17ad8096a4fc6bf1f43b7e67a85ec59a92177
Egor Kliutsuk <57603709+Egor-kl@users.noreply.github.com>
2020-11-10 08:31:58 +0300

    feat: add Selection Sort algorithm

--- Grokking-Algorithms/Chapter-2/SelectionSort.cs
+++ Grokking-Algorithms/Chapter-2/SelectionSort.cs
@@ -1,7 +1,38 @@
-﻿namespace Chapter_2
+﻿using System.Collections.Generic;
+
+namespace Chapter_2
 {
     public class SelectionSort
     {
+        public int[] SelectionSorting(List<int> arr)
+        {
+            var newArr = new int[arr.Count];
 
+            for (int i = 0; i < newArr.Length; i++)
+            {
+                var smallest = FindSmallest(arr);
+                newArr[i] = arr[smallest];
+                arr.RemoveAt(smallest);
+            }
+
+            return newArr;
+        }
+
+        private static int FindSmallest(List<int> arr)
+        {
+            var smallest = arr[0];
+            var smallestIndex = 0;
+
+            for (int i = 0; i < arr.Count; i++)
+            {
+                if (arr[i] < smallest)
+                {
+                    smallest = arr[i];
+                    smallestIndex = i;
+                }
+            }
+
+            return smallestIndex;
+        }
     }
 }
--- Grokking-Algorithms/Console-Start/Program.cs
+++ Grokking-Algorithms/Console-Start/Program.cs
@@ -1,5 +1,7 @@
 ﻿using Binary_Search;
+using Chapter_2;
 using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 
 namespace Grokking_Algorithms
@@ -15,7 +17,7 @@ namespace Grokking_Algorithms
                          26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50,
                          51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75,
                          76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100};
-
+            var smallArr = new List<int>() { 4, 3, 8, 1, 9, 5, 2, 6, 7 };
             #endregion
 
 
@@ -45,6 +47,8 @@ namespace Grokking_Algorithms
             #region
 
             stopwatch.Start();
+            SelectionSort selectionSort = new SelectionSort();
+            Console.WriteLine(string.Join(", ", selectionSort.SelectionSorting(smallArr)));
             stopwatch.Stop();
             Console.WriteLine($"Время выполнения сортировки выбором: {stopwatch.Elapsed}"); //Совет: запустите несколько раз и примерно найдите среднее.
             stopwatch.Reset();
