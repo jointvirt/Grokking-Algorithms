5878a5edca87567f1f930e9e3f90cd5f59be2908
Egor Kliutsuk <57603709+Egor-kl@users.noreply.github.com>
2020-11-21 23:31:46 +0300

    feat: add tests for chapter-2

--- Grokking-Algorithms.Tests/GrokkingAlgorithms.Tests/Chapter2Test.cs
+++ Grokking-Algorithms.Tests/GrokkingAlgorithms.Tests/Chapter2Test.cs
@@ -1,8 +1,22 @@
-﻿using NUnit.Framework;
+﻿using Chapter_2;
+using NUnit.Framework;
+using System.Collections.Generic;
 
 namespace GrokkingAlgorithms.Tests
 {
     class Chapter2Test
     {
+        private readonly SelectionSort selectionSort = new SelectionSort();
+        private static readonly object[] _sourceLists =
+        {
+            new object[] {new List<int> {2, 3, 1}, new int[] { 1, 2, 3 } },   
+            new object[] {new List<int> {5, 6, 1, 4, 3, 2, 7, 9, 8}, new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 } } 
+        };
+
+        [Test, TestCaseSource("_sourceLists")]
+        public int[] SelectionSort_SelectionSorting_ReturnsExceptedArray(List<int> list)
+        {
+            return selectionSort.SelectionSorting(list);
+        }
     }
 }
