94b9349b8ebbd559c2777b1e3545ea6f876f1b0d
Egor Kliutsuk <57603709+Egor-kl@users.noreply.github.com>
2020-11-22 07:17:41 +0300

    feat: add tests for rekursion

--- Grokking-Algorithms.Tests/GrokkingAlgorithms.Tests/Chapter3Test.cs
+++ Grokking-Algorithms.Tests/GrokkingAlgorithms.Tests/Chapter3Test.cs
@@ -1,10 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Chapter_3;
+using NUnit.Framework;
 
 namespace GrokkingAlgorithms.Tests
 {
     class Chapter3Test
     {
+        private readonly Rekursion _rekursion = new Rekursion();
+        
+        [TestCase(1, ExpectedResult = 1)]
+        [TestCase(2, ExpectedResult = 2)]
+        [TestCase(3, ExpectedResult = 6)]
+        [TestCase(4, ExpectedResult = 24)]
+        [TestCase(5, ExpectedResult = 120)]
+        public int? Rekursion_Factorial_ReturnsExceptedNumber(int n)
+        {
+            return _rekursion.Factorial(n);
+        }
     }
 }
