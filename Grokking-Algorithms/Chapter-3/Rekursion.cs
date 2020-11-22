5c33fc1e512a22723f1e1d61cbcd0d3af3a9a70b
Egor Kliutsuk <57603709+Egor-kl@users.noreply.github.com>
2020-11-22 07:18:20 +0300

    feat: update rekursion method

---
+++ Grokking-Algorithms/Chapter-3/Rekursion.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Chapter_3
+{
+    public class Rekursion
+    {
+        public int? Factorial(int n)
+        {
+            if (n == 0)
+            {
+                return 1;
+            }
+            else
+            {
+                return n * Factorial(n - 1);
+            }
+        }
+    }
+}
