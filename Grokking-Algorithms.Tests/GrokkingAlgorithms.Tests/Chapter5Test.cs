5613417c4dcffe425e59ca7827f62c7e28feead5
Egor Kliutsuk <57603709+Egor-kl@users.noreply.github.com>
2020-11-30 08:16:37 +0300

    feat: add unit test for chapter 5

--- Grokking-Algorithms.Tests/GrokkingAlgorithms.Tests/Chapter5Test.cs
+++ Grokking-Algorithms.Tests/GrokkingAlgorithms.Tests/Chapter5Test.cs
@@ -1,7 +1,20 @@
-﻿namespace GrokkingAlgorithms.Tests
+﻿using Chapter_5;
+using NUnit.Framework;
+
+namespace GrokkingAlgorithms.Tests
 {
     public class Chapter5Test
     {
-        
+        private readonly HashTables _hashTables = new HashTables();
+
+        [TestCase("Egor", ExpectedResult = "let them vote!")]
+        [TestCase("Egor", ExpectedResult = "kick them out!" )]
+        [TestCase("Roma", ExpectedResult = "let them vote!")]
+        [TestCase("Darya", ExpectedResult = "let them vote!")]
+        [TestCase("Roma", ExpectedResult = "kick them out!" )]
+        public string BinarySearch_Binary_ReturnsExceptedNumber(string name)
+        {
+            return _hashTables.Check(name);
+        }
     }
 }
