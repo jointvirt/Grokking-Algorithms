cb3c46bf0ac221311415c2e1a0e3ac0a6767ee2f
Egor Kliutsuk <57603709+Egor-kl@users.noreply.github.com>
2020-12-09 13:14:59 +0300

    feat: add unit tests

--- Grokking-Algorithms.Tests/GrokkingAlgorithms.Tests/Chapter7Test.cs
+++ Grokking-Algorithms.Tests/GrokkingAlgorithms.Tests/Chapter7Test.cs
@@ -1,10 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using Chapter_7;
+using NUnit.Framework;
 
 namespace GrokkingAlgorithms.Tests
 {
     class Chapter7Test
     {
+        private readonly FindLowest find = new FindLowest();
+
+        [TestCase(ExpectedResult = "[a, 5], [b, 2], [fin, 6]")]
+        public string FindLowest_Find_ReturnString()
+        {
+            return find.Find();
+        }
     }
 }
